12c5c985577d190a75dd6cbd4540b7cceb7fae5c
James P. Galovic <jpgalovic@gmail.com>
2020-08-20 02:06:10 +1000

    Add: GL Middleware

--- EntropyGraphics/Window.cpp
+++ EntropyGraphics/Window.cpp
@@ -1,8 +1,44 @@
 #include "Window.h"
 
-using namespace Entropy;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-Window::Window(const char* title)
+#include <Log.h>
+
+Entropy::Window::Window(const char* title, unsigned int width, unsigned int height, unsigned int numSamples)
 {
+	initGLFW(numSamples)
+}
 
+void Entropy::Window::bind()
+{
+}
+
+void Entropy::Window::clear()
+{
+}
+
+void Entropy::Window::processEvents()
+{
+}
+
+bool Entropy::Window::getShouldClose()
+{
+	return false;
+}
+
+bool Entropy::Window::initGLFW(unsigned int numSamples)
+{
+	if (!glfwInit()) {
+		Entropy::log::error("Failed to inialize GLFW.");
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef ENTROPY_PLATFORM_APPLE
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+	glfwWindowHint(GLFW_SAMPLES, numSamples);
 }
--- EntropyGraphics/Window.h
+++ EntropyGraphics/Window.h
@@ -5,6 +5,16 @@
 namespace Entropy {
 	class ENTROPY_GRAPHICS_API Window
 	{
-		Window(const char* title);
+	public:
+		Window(const char* title, unsigned int width = 800U, unsigned int height = 600U, unsigned int numSamples = 4U);
+
+		void bind();
+		void clear();
+		void processEvents();
+
+		bool getShouldClose();
+
+	private:
+		bool initGLFW(unsigned int numSamples);
 	};
 }
