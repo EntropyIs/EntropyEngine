ca77d28ad0b88b1541f37465524ccdd24263a5b5
J.P.Galovic <jpgalovic@gmail.com>
2021-04-16 11:45:56 +1000

    Fix: Logging Calls

--- src/Sandbox/Sandbox.cpp
+++ src/Sandbox/Sandbox.cpp
@@ -6,16 +6,16 @@
 class ExampleLayer : public Entropy::Layer
 {
 public:
-	ExampleLayer() : Layer("Example") {}
+	ExampleLayer() : Layer("EXAMPLE_LAYER") {}
 
 	void OnUpdate() override
 	{
-		Entropy::log::message("ExampleLayer::Update");
+		Entropy::log::message("EXAMPLE_LAYER","Update");
 	}
 
 	void OnEvent(Entropy::Event& e) override
 	{
-		Entropy::log::trace(GetName().c_str(), e);
+		Entropy::log::trace("EXAMPLE_LAYER", e);
 	}
 };
 
